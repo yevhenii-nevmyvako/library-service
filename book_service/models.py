324aae7b2a12125ca7ac00ba7ffb6c97eaae0226
eugene <bantyc8@gmail.com>
2023-02-03 17:39:27 +0200

    Implementation Book model in boor_service & first migration

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=5, choices=CoverChoices.choices)
+    inventory = models.IntegerField(
+        validators=[MinValueValidator(0)],
+        default=0,
+    )
+    daily_free = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0), MaxValueValidator(99999)],
+        default=0,
+    )
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Books"
+        verbose_name_plural = "Books"
+
+    def __str__(self) -> str:
+        return self.title
