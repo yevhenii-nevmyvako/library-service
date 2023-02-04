87c8dc89a0e80a536d5cbacc208dce2e4219c788
eugene <bantyc8@gmail.com>
2023-02-04 17:28:51 +0200

    Initialized User serializers with AuthTokenSerializer with custom changes in log-in api forms & Add urls with user endpoints to PR & Mare migrate to db

--- library_service/urls.py
+++ library_service/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("book_service.urls", namespace="books")),
+    path("api/users/", include("user.urls", namespace="users")),
 ]
